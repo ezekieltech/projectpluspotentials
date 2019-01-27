9b3fee251d554fd2ecc6b37719c3d2b42484266d
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-01-27 07:34:18 +0100

    test

--- website/websiteapp/urls.py
+++ website/websiteapp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #    url(r'^$', views.index, name='index'),
 ]
 
 # used like this: <a href="{% url 'index' %}">Home</a> on the template
