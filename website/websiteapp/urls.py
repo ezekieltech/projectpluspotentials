6e67e8d22db1fac74f9e6702b0908404c54e6b0c
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-01-27 12:18:37 +0100

    simple views,url,template set up

--- website/websiteapp/urls.py
+++ website/websiteapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^projects/$', views.ProjectListView.as_view(), name='projects'),
+    url(r'^project/(?P<pk>\d+)$', views.ProjectDetailView.as_view(), name='project-detail'),
 ]
 
 # used like this: <a href="{% url 'index' %}">Home</a> on the template
