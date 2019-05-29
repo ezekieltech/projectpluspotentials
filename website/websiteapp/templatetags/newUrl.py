6ba198b2fa89f9294e2b4cddec095318570d99b1
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2019-05-29 14:34:17 +0100

    Restored Project to Best State
    Final Content correction, projects page update

--- website/websiteapp/templatetags/newUrl.py
+++ website/websiteapp/templatetags/newUrl.py
@@ -1,27 +1,9 @@
+# <app>/templatetags/my_tags.py
 from django import template
 
 register = template.Library()
 
 
-@register.filter
-def addstr(arg1, arg2):
-    """concatenate arg1 & arg2"""
-    special_letters = ['&', '/', ' ']
-    for i in special_letters:
-        arg1 = str(arg1).replace(i, '')
-        arg2 = str(arg2).replace(i, '')
-    return arg1 + arg2
-
-
-@register.filter
-def mycounter(a):
-    """variable to hold counter"""
-    mycount = 0
-    mycount = + 1
-    return mycount
-
-
-# <app>/templatetags/my_tags.py
 @register.simple_tag(takes_context=True)
 def param_replace(context, **kwargs):
     """
